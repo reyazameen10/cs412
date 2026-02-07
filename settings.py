7d379c625adbfe1b524fa53f6965925dc49fc79c
reyazameen10 <reyaz.am10@gmail.com>
2026-02-07 12:33:19 -0500

    Last changes in the restaurant app

--- settings.py
+++ settings.py
@@ -38,7 +38,7 @@ INSTALLED_APPS = [
     'django.contrib.sessions',
     'django.contrib.messages',
     'django.contrib.staticfiles',
-    "restaurant",  #new app
+    'restaurant',  #new app
 ]
 
 MIDDLEWARE = [
