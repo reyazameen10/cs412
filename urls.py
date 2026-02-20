09f1f05f3d6a222f0bad50f4a32a4330e8380e9f
reyazameen10 <reyaz.am10@gmail.com>
2026-02-20 16:40:39 -0500

    added mini_insta

--- urls.py
+++ urls.py
@@ -16,6 +16,8 @@ Including another URLconf
 """
 from django.contrib import admin
 from django.urls import path, include
+from django.conf import settings
+from django.conf.urls.static import static
 
 urlpatterns = [
     path(r'admin/', admin.site.urls),
@@ -24,3 +26,6 @@ urlpatterns = [
     path('mini_insta/', include('mini_insta.urls')),#New 
 ]
 
+if settings.DEBUG:
+
+    urlpatterns += static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
